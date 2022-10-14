ee593a66af366d57e07d5e6423add677b83e6a1a
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2022-10-14 23:35:05 +0800

    Sinkronisasi database baru

--- play.php
+++ play.php
@@ -1,8 +1,12 @@
 <?php
 require "func/koneksi.php";
 $id = $_GET["id"];
-$query = mysqli_query($kon, "SELECT * FROM quiz WHERE id='$id'");
+$query = mysqli_query($kon, "SELECT *, soal.id AS soal_id FROM soal INNER JOIN quis ON soal.id_quis=quis.id WHERE id_quis=$id");
 $data = mysqli_fetch_array($query);
+
+// echo "<pre>";
+// var_dump($data);
+// die();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,41 +34,19 @@ $data = mysqli_fetch_array($query);
                     <p class="text-muted"><?= $data["deskripsi"] ?></p>
 
                     <form action="" method="POST">
-                        <p><?= $data["quiz1"] ?> :</p>
-                        <input type="text" name="jawaban1" id="jawaban1" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <br>
-                        <p><?= $data["quiz2"] ?> :</p>
-                        <input type="text" name="jawaban2" id="jawaban2" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <p><?= $data["quiz3"] ?> :</p>
-                        <input type="text" name="jawaban3" id="jawaban3" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <br>
-                        <p><?= $data["quiz4"] ?> :</p>
-                        <input type="text" name="jawaban4" id="jawaban4" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <br>
-                        <p><?= $data["quiz5"] ?> :</p>
-                        <input type="text" name="jawaban5" id="jawaban5" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <br>
-                        <p><?= $data["quiz6"] ?> :</p>
-                        <input type="text" name="jawaban6" id="jawaban6" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <br>
-                        <p><?= $data["quiz7"] ?> :</p>
-                        <input type="text" name="jawaban7" id="jawaban7" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <p><?= $data["quiz8"] ?> :</p>
-                        <input type="text" name="jawaban7" id="jawaban7" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <p><?= $data["quiz9"] ?> :</p>
-                        <input type="text" name="jawaban7" id="jawaban7" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
-                        <p><?= $data["quiz10"] ?> :</p>
-                        <input type="text" name="jawaban7" id="jawaban7" class="form-control" autocomplete="off" placeholder="Enter Answer">
-                        <br><br>
+                        <?php foreach ($query as $soal): ?>
+                        <p><?= $soal['soal'] ?> :</p>
+                            <input type="text" name="jawaban[]" id="jawaban[]" class="form-control" autocomplete="off" placeholder="Enter Answer">
+                            <input type="hidden" name="soal_id[]" value="<?= $soal['soal_id'] ?>">
+                            <input type="hidden" name="poin[]" value="<?= $soal['poin'] ?>">
+                                <br><br>
+                            <br> 
+                            <?php 
+                                $valid_soal_id[] = $soal['soal_id'];
+                                $valid_poin[] = $soal['poin'];
+                            ?>
+                        <?php endforeach ?>
+                        
                         <button type="submit" name="submit" class="btn btn-outline-dark">Submit</button>
 
                     </form>
@@ -75,106 +57,70 @@ $data = mysqli_fetch_array($query);
                 <div class="col-md-6">
                     <br><br><br><br><br><br>
                     <?php
-                    if (isset($_POST["submit"])) {
+                    $quis_id = $_GET['id'];
+                    $query = mysqli_query($kon, "SELECT * FROM point WHERE quis_id=$quis_id AND user_id=1");
+                    $user_poin = mysqli_fetch_array($query);
 
-
-                        $jawaban1 = mysqli_real_escape_string($kon, $_POST["jawaban1"]);
-                        $jawaban2 = mysqli_real_escape_string($kon, $_POST["jawaban2"]);
-                        $jawaban3 = mysqli_real_escape_string($kon, $_POST["jawaban3"]);
-                        $jawaban4 = mysqli_real_escape_string($kon, $_POST["jawaban4"]);
-                        $jawaban5 = mysqli_real_escape_string($kon, $_POST["jawaban5"]);
-                        $jawaban6 = mysqli_real_escape_string($kon, $_POST["jawaban6"]);
-                        $jawaban7 = mysqli_real_escape_string($kon, $_POST["jawaban7"]);
-                        $jawaban8 = mysqli_real_escape_string($kon, $_POST["jawaban8"]);
-                        $jawaban9 = mysqli_real_escape_string($kon, $_POST["jawaban9"]);
-                        $jawaban10 = mysqli_real_escape_string($kon, $_POST["jawaban10"]);
-
-                        $correct = 0;
-
-                        $jawaban_db_1 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban1 = '$jawaban1'"));
-                        if ($jawaban_db_1) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                            Duh, jawabanmu yang nomor 1 salah nih.
-                        </div>";
-                        }
-                        $jawaban_db_2 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban2 = '$jawaban2'"));
-                        if ($jawaban_db_2) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 2 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_3 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban3 = '$jawaban3'"));
-                        if ($jawaban_db_3) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 3 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_4 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban4 = '$jawaban4'"));
-                        if ($jawaban_db_4) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 4 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_5 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban5 = '$jawaban5'"));
-                        if ($jawaban_db_5) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 5 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_6 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban6 = '$jawaban6'"));
-                        if ($jawaban_db_6) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 6 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_7 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban7 = '$jawaban7'"));
-                        if ($jawaban_db_7) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 7 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_8 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban8 = '$jawaban8'"));
-                        if ($jawaban_db_7) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 8 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_9 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban9 = '$jawaban9'"));
-                        if ($jawaban_db_9) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 9 salah nih.
-                                    </div>";
-                        }
-                        $jawaban_db_10 = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM quiz WHERE jawaban10 = '$jawaban10'"));
-                        if ($jawaban_db_10) {
-                            $correct += 10;
-                        } else {
-                            echo "<div class='alert alert-danger' role='alert'>
-                                        Duh, jawabanmu yang nomor 10 salah nih.
-                                    </div>";
-                        }
+                    if ($user_poin == TRUE) {
                         echo "<div class='alert alert-success' role='alert'>
-                                        Mantap, kamu mendapatkan $correct point
-                                    </div>";
+                            Poin kamu dikuis ini adalah".$user_poin['total']."
+                        </div>";
                     }
+
+                    if (isset($_POST['submit'])) {
+                        $jawaban = $_POST['jawaban'];
+                        $soal_id = $_POST['soal_id'];
+                        $point = $_POST['poin'];
+                        $user_id = 1;
+                        $jml_soal = count($jawaban);
+
+                        $query = mysqli_query($kon, "UPDATE point SET
+                                total = 0
+                            WHERE quis_id=$quis_id AND user_id=1
+                        ");
+
+                        for ($i=0; $i < $jml_soal; $i++) {
+                            if ($point[$i] != $valid_poin[$i] || $soal_id != $valid_soal_id) {
+                                exit('Terjadi kesalahan pada server');
+                            }else{
+
+                                $query_insert = mysqli_query($kon, "INSERT INTO jawab(jawaban,soal_id,user_id) VALUES ('$jawaban[$i]','$soal_id[$i]','$user_id')");
+                                    
+                                if (!$query_insert) {
+                                    echo 'Terjadi kesalahan pada server';
+                                }else{
+
+                                    $query = mysqli_fetch_array(mysqli_query($kon, "SELECT * FROM soal WHERE jawab='$jawaban[$i]'"));
+                                        if (!$query) {
+                                            $point_benar = 0;
+                                            $no = $i + 1;
+                                            echo "<div class='alert alert-danger' role='alert'>
+                                                Duh, jawabanmu yang nomor ".$no." salah nih.
+                                            </div>";
+
+                                        }else{
+                                            // echo 'ada datanya';
+                                            $point_benar = $_POST['poin'];
+                                            $query = mysqli_query($kon, "SELECT * FROM point WHERE quis_id=$quis_id AND user_id=1");
+                                            $data_poin = mysqli_fetch_array($query);
+                                            if ($data_poin == null) {
+                                                $query = mysqli_query($kon, "INSERT INTO point(total,quis_id,user_id) VALUES ('$point_benar[$i]','$quis_id','$user_id')");
+                                            }else{
+                                                $total[$i] = $data_poin['total'] + $point_benar[$i];
+                                                $query = mysqli_query($kon, "UPDATE point SET
+                                                     total = '$total[$i]'
+                                                     WHERE quis_id=$quis_id AND user_id=1
+                                                 ");
+                                                if (!$query) {
+                                                    exit('Terjadi kesalahan pada server');
+                                                }
+                                            }
+                                        }
+                                }
+                            }
+                        }
+                    }
+
                     ?>
                 </div>
             </div>
@@ -182,7 +128,4 @@ $data = mysqli_fetch_array($query);
     </div>
     <script src="js/script.js"></script>
 </body>
-
-
-
 </html>
